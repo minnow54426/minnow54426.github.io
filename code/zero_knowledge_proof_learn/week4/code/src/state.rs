9fb39b2fc79d4b9d51c5d696605f7a1619507acf
boycrypt <minnow54426@gmail.com>
2026-01-04 22:15:21 +0800

    feat: add State with account storage

--- code/zero_knowledge_proof_learn/week4/code/src/state.rs
+++ code/zero_knowledge_proof_learn/week4/code/src/state.rs
@@ -1,6 +1,8 @@
 // State module placeholder
 
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use ed25519_dalek::PublicKey;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Account {
@@ -11,6 +13,27 @@ pub struct Account {
 impl Account {
     pub fn new(balance: u64, nonce: u64) -> Self {
         Self { balance, nonce }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct State {
+    accounts: HashMap<[u8; 32], Account>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            accounts: HashMap::new(),
+        }
+    }
+
+    pub fn get_account(&self, pubkey: &PublicKey) -> Option<&Account> {
+        self.accounts.get(pubkey.as_bytes())
+    }
+
+    pub fn set_account(&mut self, pubkey: PublicKey, account: Account) {
+        self.accounts.insert(*pubkey.as_bytes(), account);
     }
 }
 
@@ -31,5 +54,26 @@ mod tests {
         let account = Account::new(100, 5);
         let json = serde_json::to_string(&account).unwrap();
         assert_eq!(json, r#"{"balance":100,"nonce":5}"#);
+    }
+
+    #[test]
+    fn test_state_get_and_set() {
+        use ed25519_dalek::SecretKey;
+        use rand::rngs::OsRng;
+
+        let mut state = State::new();
+        let key = SecretKey::generate(&mut OsRng);
+        let pubkey: PublicKey = (&key).into();
+
+        // Get non-existent account
+        let account = state.get_account(&pubkey);
+        assert_eq!(account, None);
+
+        // Set account
+        state.set_account(pubkey, Account::new(100, 0));
+
+        // Get account
+        let account = state.get_account(&pubkey);
+        assert_eq!(account, Some(&Account::new(100, 0)));
     }
 }
